03308bce3bbdf8269d5f62311c8027fe2405dc50
wuzel <marg2.wilkinson@gmail.com>
2016-04-02 19:24:58 +1000

    Added files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express()
+
+app.get('/', function (req, res){
+    res.send('Hello there,world!')
+})
+
+app.get('/yo', function (req,res){
+    res.send('Yo!')
+})
+
+var server = app.listen(3000, function () {
+  console.log('Example app listening on port 3000! ' + server.address().port);
+});
+
